d40b11254e0d0f9d429e9ccf00af341047ef5e02
Karan <karanbhatia@protonmail.com>
2021-05-18 10:24:51 +0530

    Added Logs

--- appv2.py
+++ appv2.py
@@ -2,11 +2,14 @@ import datetime
 import json
 import numpy as np
 import requests
-import pandas as pd
-from copy import deepcopy
+import logging
 from fake_useragent import UserAgent
 from pushbullet.pushbullet import PushBullet
 from configparser import ConfigParser
+
+logging.basicConfig(filename="newfile.log", format='%(asctime)s %(message)s', filemode='a')
+logger=logging.getLogger()
+logger.setLevel(logging.INFO)
 
 numdays = 7
 DIST_ID = '170'
@@ -17,23 +20,6 @@ auth = config_object["AUTH"]["api_key"]
 print(auth)
 p = PushBullet(auth)
 
-def load_mapping(): 
-    df = pd.read_csv("district_mapping.csv")
-    return df
-
-def filter_column(df, col, value):
-    df_temp = deepcopy(df.loc[df[col] == value, :]) 
-    return df_temp
-
-def filter_capacity(df, col, value):
-    df_temp = deepcopy(df.loc[df[col] > value, :])
-    return df_temp
-
-mapping_df = load_mapping() 
-
-valid_states = list(np.unique(mapping_df["state_name"].values))
-
-
 base = datetime.datetime.today()
 date_list = [base + datetime.timedelta(days=x) for x in range(numdays)]
 date_str = [x.strftime("%d-%m-%Y") for x in date_list]
@@ -41,7 +27,6 @@ date_str = [x.strftime("%d-%m-%Y") for x in date_list]
 temp_user_agent = UserAgent()
 browser_header = {'User-Agent': temp_user_agent.random}
 
-final_df = None
 for INP_DATE in date_str:
     URL = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id={}&date={}".format(DIST_ID, INP_DATE)
     response = requests.get(URL, headers=browser_header)
@@ -51,6 +36,7 @@ for INP_DATE in date_str:
             for mydistrictcenters in resp_json:
                 if(((mydistrictcenters['pincode']==370205) or (mydistrictcenters['pincode']==370201))):
                     for availablesessions in mydistrictcenters['sessions']:
+                        logger.info(" : Records Fetched")
                         if((availablesessions['min_age_limit']==18) and (availablesessions['available_capacity']==0)):
                             text = 'Vaccine Unavailable'
                             body = 'Go back to sleep'
@@ -58,6 +44,8 @@ for INP_DATE in date_str:
                             text = 'Vaccine Available'
                             body = 'GO GO GO!!!'
         else:
-            st.error("No rows in the data Extracted from the API")
+            logger.info(" : No rows in the data Extracted from the API")
+    else:
+        logger.info(" : API Unaccessible")
 
 p.pushNote(p.getDevices()[0]["iden"], text, body)
